d2a89816084385843dc612d6839d1769f13d3935
robert-bugean <robert.bugean01@gmail.com>
2023-08-13 14:11:00 +0200

    auto-insert company_customer

--- cloudmark/src/main/resources/data.sql
+++ cloudmark/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO company (company_name, vat_number, address, iban, phone_number, email, pec, fax)
-vaLUES
+VALUES
   ("azienda 1", "it99131003460", "via firenze 84", "it22m0300203280982955485721", "0487925101", "info@azienda1.com", "azienda1@pec.com", NULL),
   ("azienda 2", "it87656182089", "via callicratide 127", "it79g0300203280493833225987", "0982467855", "info@azienda2.com", "azienda2@pec.com", NULL),
   ("azienda 3", "it11485751687", "via giacinto gigante 60", "it06g0300203280941962747712", "0489272045", "info@azienda3.com", "azienda3@pec.com", NULL),
@@ -13,7 +13,7 @@ vaLUES
 
 
 INSERT INTO customer (customer_name, vat_number, address, cap, city, iban, phone_number, email, pec, fax)
-vaLUES
+VALUES
   ("cliente 1", "it19153550359", "via varrone 81", "95121", "catania", "it08n0300203280861231471683", "3304872683", "cliente1@email.com", "cliente1@pec.com", NULL),
   ("cliente 2", "it97452220124", "via zannoni 49", "38011", "trento", "it53y0300203280194131412944", "3434286622", "cliente2@email.com", "cliente2@pec.com", NULL),
   ("cliente 3", "it13266385367", "via del carmine 145", "36060", "vicenza", "it03l0300203280652262656572", "3522700838", "cliente3@email.com", "cliente3@pec.com", NULL),
@@ -24,3 +24,21 @@ vaLUES
   ("cliente 8", "it27833252354", "via santa teresa 17", "96010", "siracusa", "it24x0300203280888788363426", "3498547323", "cliente8@email.com", "cliente8@pec.com", NULL),
   ("cliente 9", "it33091429285", "via piave 133", "66030", "chieti", "it67p0300203280351252564847", "3126439669", "cliente9@email.com", "cliente9@pec.com", NULL),
   ("cliente 10", "it26939645679", "via guantai nuovi 31", "80030", "napoli", "it28f0300203280484683778657", "3569065195", "cliente10@email.com", "cliente10@pec.com", NULL);
+
+INSERT INTO company_customer (company_id, customer_id)
+VALUES
+  (1, 5),
+  (2, 8),
+  (2, 1),
+  (3, 6),
+  (3, 4),
+  (4, 9),
+  (5, 7),
+  (6, 10),
+  (7, 2),
+  (7, 5),
+  (8, 3),
+  (8, 6),
+  (9, 4),
+  (9, 10),
+  (10, 1);
